6d995e5d7e6f3b03c49bb7ec696e586b3e3edf4a
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-07 17:16:15 +0530

    refactor Navbar05 component; replace anchor tags with divs for navigation using useNavigate

--- frontend/utils/Navbar05/Navbar05.jsx
+++ frontend/utils/Navbar05/Navbar05.jsx
@@ -1,5 +1,6 @@
 // Navbar04.jsx
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const defaultLinks = [
   { name: "Home", link: "/" },
@@ -9,15 +10,16 @@ const defaultLinks = [
 ];
 
 const Navbar05 = ({ links = defaultLinks }) => {
+  const navigate = useNavigate();
   return (
     <div
       className="fixed top-2 left-1/2 z-30 pointer-events-auto bg-black bg-opacity-80 backdrop-blur-md cursor-default flex items-center justify-center p-2 rounded-full shadow-[0_0_10px_rgba(255,255,255,0.8)]"
       style={{ transform: "translateX(-50%)", marginTop: "10px" }}
     >
       {links.map((link, i) => (
-        <a key={i} href={link.link} className="bg-black text-white px-3 py-1 rounded-full text-xs shadow-md">
+        <div key={i} onClick={()=>navigate(link.link)} className="bg-black text-white px-3 py-1 cursor-pointer rounded-full text-xs shadow-md">
           {link.name}
-        </a>
+        </div>
       ))}
     </div>
   );
